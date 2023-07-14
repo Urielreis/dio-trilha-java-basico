d71ee62fd6497d70f06d7b86b9470d4a26ee7836
Urielreis <urielreis1992@yahoo.com.br>
2023-07-14 00:20:06 -0300

    Projeto terminado

--- Conta-Banco/Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da conta:");
+        numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o número da agência:");
+        agencia = sc.next();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o saldo da conta:");
+        saldo = sc.nextDouble();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
     }
 }
